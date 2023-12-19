95049b1daf9044a0fd342b479ba92c481c1d0132
mathias-crochet <mathias.crochet@estiam.com>
2023-12-19 12:27:01 +0100

    dictionnaire

---
+++ go.mod
@@ -0,0 +1,3 @@
+module dictionnaire
+
+go 1.21.5
---
+++ main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+    "fmt"
+)
+
+func main() {
+
+    m := make(map[string]int)
+
+    m["a"] = 2
+    m["b"] = 4
+	m["c"] = 6
+
+    fmt.Println("map:", m)
+
+	// get
+    a := get(m, "k1")
+	fmt.Println(a)
+
+	//list
+	list(m)
+
+	//remove
+	remove(m, "c")
+    fmt.Println("map sans 'c' :", m)
+	
+}
+
+func get(dictionnaire map[string]int, reference string) int {
+	return dictionnaire[reference]
+}
+
+func list(dictionnaire map[string]int) {
+	fmt.Println("Dictionnaire :")
+	for key, value := range dictionnaire {
+		fmt.Printf("%s: %d\n", key, value)
+	}
+}
+
+func remove(dictionnaire map[string]int, reference string) {
+	delete(dictionnaire, reference)
+}
