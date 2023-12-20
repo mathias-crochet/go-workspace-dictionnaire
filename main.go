594a5ec37b5dfb6baa5f2edca842198a16882dab
mathias-crochet <mathias.crochet@estiam.com>
2023-12-20 12:51:26 +0100

    goroutine and channel

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"dictionnaire/dictionary"
+	"fmt"
 )
 
 func main() {
@@ -11,14 +12,35 @@ func main() {
 	langageGo := dictionary.NewDefinition("go", "langage de programmation compilé créé par Google")
 	estiam := dictionary.NewDefinition("estiam", "École supérieure des technologies de l’information appliquées aux métiers")
 
-	dictionary.Add(langageGo, file)
-	dictionary.Add(estiam, file)
+	addChannel := make(chan string)
+	removeChannel := make(chan string)
+
+	go func()  {
+		dictionary.Add(langageGo, file)
+		addChannel <- "la définition a été ajoutée"
+	}()
+
+    fmt.Println(<-addChannel)
+
+	go func()  {
+		dictionary.Add(estiam, file)
+		addChannel <- "la définition a été ajoutée"
+	}()
+
+    fmt.Println(<-addChannel)
+
 	
+	go func()  {
+		dictionary.Remove("estiam", file)
+		removeChannel <- "la définition a été supprimé"
+	}()
+
+    fmt.Println(<-removeChannel)
+
+		
 	dictionary.Get("go", file)
-
 	dictionary.List(file)
-
-	dictionary.Remove("estiam", file)
+	
 	dictionary.List(file)
 	
 
